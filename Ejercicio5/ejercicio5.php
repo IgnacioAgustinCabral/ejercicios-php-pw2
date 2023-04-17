34ab979bd49a67132c5f0b3f6e19de0407389e59
Ignacio Agustin Cabral <ignaciocabral97@gmail.com>
2023-04-16 21:58:39 -0300

    ejercicio 5

---
+++ Ejercicio5/ejercicio5.php
@@ -0,0 +1,31 @@
+<?php
+function sumatoria_a($array)
+{
+    $resultado = 0;
+
+    for ($i = 0; $i < count($array); $i++) {
+        $resultado += $array[$i];
+    }
+    return $resultado;
+
+}
+
+function sumatoria_b($array)
+{
+    $resultado = 0;
+    foreach ($array as $item) {
+        $resultado += $item;
+    }
+    return $resultado;
+}
+
+function sumatoria_c($array)
+{
+    $resultado = 0;
+    $i = 0;
+    while ($i < count($array)) {
+        $resultado += $array[$i];
+        $i++;
+    }
+    return $resultado;
+}
--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require_once('Ejercicio1/ejercicio1.php');
 require_once('Ejercicio2/ejercicio2.php');
 require_once('Ejercicio3/ejercicio3.php');
 require_once('Ejercicio4/ejercicio4.php');
+require_once('Ejercicio5/ejercicio5.php');
 
 //Ejercicio 1
 echo semaforo_a("rojo") . "\n";
@@ -21,3 +22,10 @@ $numero = 41;
 echo 'numero antes de la llamada a la función: ' . $numero . "\n";
 incrementar($numero);
 echo 'numero después de la llamada a la función: ' . $numero . "\n";
+
+//Ejercicio 5
+$array = array(1, 2, 3, 4, 5);
+
+echo sumatoria_a($array) . "\n";
+echo sumatoria_b($array) . "\n";
+echo sumatoria_c($array) . "\n";
