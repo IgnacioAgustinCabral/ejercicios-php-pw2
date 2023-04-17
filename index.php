83c7e76a43600be9cc4598b055b25ce4ff52a659
Ignacio Agustin Cabral <ignaciocabral97@gmail.com>
2023-04-16 21:29:58 -0300

    ejercicio 3

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once('Ejercicio1/ejercicio1.php');
 require_once('Ejercicio2/ejercicio2.php');
+require_once('Ejercicio3/ejercicio3.php');
 
 //Ejercicio 1
 echo semaforo_a("rojo") . "\n";
@@ -10,3 +11,6 @@ echo semaforo_c("verde") . "\n";
 //Ejercicio 2
 echo binomioCuadradoPerfecto_a(2, 2) . "\n";
 echo binomioCuadradoPerfecto_b(2, 2) . "\n";
+
+//Ejercicio 3
+echo concatenar("Hola me llamo", "Ignacio");
