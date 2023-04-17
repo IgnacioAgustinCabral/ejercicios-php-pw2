0b77a46d16355db781db9bda8651461c4ade5a55
Ignacio Agustin Cabral <ignaciocabral97@gmail.com>
2023-04-16 21:40:11 -0300

    ejercicio 4

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once('Ejercicio1/ejercicio1.php');
 require_once('Ejercicio2/ejercicio2.php');
 require_once('Ejercicio3/ejercicio3.php');
+require_once('Ejercicio4/ejercicio4.php');
 
 //Ejercicio 1
 echo semaforo_a("rojo") . "\n";
@@ -13,4 +14,10 @@ echo binomioCuadradoPerfecto_a(2, 2) . "\n";
 echo binomioCuadradoPerfecto_b(2, 2) . "\n";
 
 //Ejercicio 3
-echo concatenar("Hola me llamo", "Ignacio");
+echo concatenar("Hola me llamo", "Ignacio") . "\n";
+
+//Ejercicio 4
+$numero = 41;
+echo 'numero antes de la llamada a la función: ' . $numero . "\n";
+incrementar($numero);
+echo 'numero después de la llamada a la función: ' . $numero . "\n";
